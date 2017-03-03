b3460e9c627ae34e48a87cfa9bc03a42fb1270d6
darendal <bware43@gmail.com>
2017-03-02 22:57:27 -0500

    Leftover changes from last check-in

---
+++ TripCalculatorAPI/TripCalculatorAPI/Controllers/TripCalculatorController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TripCalculatorAPI.Models;
+
+namespace TripCalculatorAPI.Controllers
+{
+    public class TripCalculatorController : ApiController
+    {
+
+        public ExpenseRepaymentCollection Post(IEnumerable<ExpenseLineItem> expenses)
+        {
+            ExpenseRepaymentCollection result = new ExpenseRepaymentCollection();
+
+
+            return result;
+        }
+
+    }
+}
